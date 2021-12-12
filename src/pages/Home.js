ae5931c034620d3ffa89e5f7ee2629d88c1c61b4
uranghishna <hishna.di@gmail.com>
2021-12-13 01:53:54 +0700

    FE desain rumah

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,20 +17,8 @@ const Home = () => {
         <div className="container">
                 <div className="row justify-content-center">
                         <div className="col-md-6">
-                        <h2>Login Sistem Desain Rumah</h2>
-                            <form onSubmit={() => alert(JSON.stringify(posts))}>
-                                <div className="form-group">
-                                    <label for="exampleInputEmail1">Username</label>
-                                     <input name="username" placeholder="Masukan Username" value={username} onChange={(e) => onChange(e)} type="email" class="form-control" required/>
-                                </div>
-                                <div className="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input name="password" placeholder="Masukan Username" value={password} onChange={(e) => onChange(e)} type="password" class="form-control" />
-                                </div>
-                                <div className="mt-4">
-                                    <button type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>   
-                                </div>
-                            </form>
+                        <h1>SELAMAT DATANG</h1>
+                        <h2>Sistem Desain Rumah</h2>
                         </div>
                 </div>
             </div> 
